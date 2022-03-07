60dd7ab08dcfd7ee209b8957ec70db48a6f82d0d
huyvu23 <vuh32616@gmail.com>
2022-03-07 16:49:34 +0700

    add

---
+++ JavaScript Basic/includes.js
@@ -0,0 +1,10 @@
+// Use String
+var title = "Responsive web design ";
+
+// ======================= chuỗi muốn tìm,số thứ tự muốn tìm chuỗi trở đi
+console.log(title.includes("web", 1));
+
+// =====================================
+// Use Array
+var courses = ["Java", "PHP", "Ruby", "Python"];
+console.log(courses.includes("PHP"));
