a4f6d504d07eedece9779b740c6377227031c996
huyvu23 <vuh32616@gmail.com>
2022-03-22 23:57:41 +0700

    add

--- JavaScript Basic/ArrayReduce.js
+++ JavaScript Basic/ArrayReduce.js
@@ -15,3 +15,22 @@ var courses = [
     coin: 150,
   },
 ];
+
+var i = 0;
+//accumulator : Biến tích trữ
+function coinHandler(accumulator, currentValue, currentIndex, originArray) {
+  i++;
+
+  var total = accumulator + currentValue.coin;
+  console.table({
+    "Lượt chạy:": i,
+    "Biến tích trữ:": accumulator,
+    "Gía khóa học:": currentValue.coin,
+    "Tích trữ được:": total,
+  });
+  return total;
+}
+
+// Đối số truyền vào là (funcion,giá trị khởi tạo)
+var totalCoin = courses.reduce(coinHandler, 0);
+console.log(totalCoin);
