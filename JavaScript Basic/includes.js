d20c535f96721aece1c50ca3a558904e2997ed39
huyvu23 <vuh32616@gmail.com>
2022-03-24 20:16:28 +0700

    add

--- JavaScript Basic/includes.js
+++ JavaScript Basic/includes.js
@@ -1,3 +1,7 @@
+// Phương thức string.includes()
+// sẽ kiểm tra xem một chuỗi con được người dúng cung cấp có nằm trong chuỗi hay không.
+// Phương thức sẽ trả về True nếu chuỗi chứa chuỗi con mà người dùng cung cấp, ngược lại sẽ trả về False.
+
 // Use String
 var title = "Responsive web design ";
 
